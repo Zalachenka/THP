6fc2cde0d11f5cf86b7d402d6db2bc65be06d57b
zalachenka <saliou.armony@gmail.com>
2019-04-12 18:30:38 +0200

    jour9

---
+++ jour9/ARGVtest/.bash_profile
@@ -0,0 +1,6 @@
+alias search="ruby /home/ton/chemin/vers/google_searcher.rb"
+alias g='git'
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit -m'
+alias ll='ls -las'
---
+++ jour9/ARGVtest/google_searcher.rb
@@ -0,0 +1,38 @@
+require 'watir'
+require 'launchy'
+browser = Watir::Browser.new()
+browser.goto 'google.com'
+
+search_bar = browser.text_field(class: 'gsfi')
+search_bar.set("#{ARGV.first}")
+
+submit_button = browser.button(type:"submit")
+submit_button.click
+
+browser.driver.manage.timeouts.implicit_wait = 3
+search_result_divs = browser.divs(class:"rc")
+search_result_divs.each { |div| p div.h3.text }
+
+puts "Bravo champion"
+browser.close
+=begin
+require 'watir'
+browser = Watir::Browser.new()
+browser.goto 'google.com'
+search_bar = browser.text_field(class: 'gsfi')
+search_bar.set("Hello World!")
+
+#méthode de la barre d'entrée
+#search_bar.send_keys(:enter)
+
+#méthode du clic
+submit_button = browser.button(type:"submit")
+submit_button.click
+
+browser.driver.manage.timeouts.implicit_wait = 3
+search_result_divs = browser.divs(class:"rc")
+search_result_divs.each { |div| p div.h3.text }
+
+p "Méfait accompli, fermeture du browser"
+browser.close
+=end
