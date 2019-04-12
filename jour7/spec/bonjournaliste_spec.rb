8ceb4f5374535c8e9208f4c7daff6db22968b309
zalachenka <saliou.armony@gmail.com>
2019-04-12 14:26:49 +0200

    doublons deleted

--- jour7/spec/bonjournaliste_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative'../lib/bonjournaliste'
-
-describe "the login_twitter method" do
-  it "should return client, and client is not nil" do
-    expect(login_twitter).not_to be_nil
-	end
-end
